02c69089ae25684dd7914c61c03348167b676fcc
e <262506529@qq.com>
2021-01-07 11:21:43 +0800

    Initial commit spring 学习项目

---
+++ src/main/java/com/xs/MainApplication.java
@@ -0,0 +1,14 @@
+package com.xs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * springboot程序 启动类
+ */
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class,args);
+    }
+}
